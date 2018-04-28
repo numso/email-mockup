af484c5aea83a8610c658b248190e03e91c61949
Dallin Osmun <dosmun@gmail.com>
2018-04-27 22:05:04 -0600

    created a sortable, filterable table of users

--- src/app.js
+++ src/app.js
@@ -1,12 +1,86 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import allUsers from './users'
+
 const Wrapper = styled.div`
   display: flex;
   margin-top: 40px;
   justify-content: center;
 `
 
-export default () => {
-  return <Wrapper>hi</Wrapper>
+const headers = [
+  { key: 'name', label: 'Name' },
+  { key: 'email', label: 'Email' },
+  { key: 'office', label: 'Office' },
+  { key: 'status', label: 'Status' }
+]
+
+export default class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      sort: 'name',
+      ascending: true,
+      filter: ''
+    }
+  }
+
+  updateSort (key) {
+    if (this.state.sort === key) {
+      this.setState({ ascending: !this.state.ascending })
+    } else {
+      this.setState({ ascending: true, sort: key })
+    }
+  }
+
+  render () {
+    const users = allUsers.filter(user => {
+      if (!this.state.filter) return true
+      return user.status === this.state.filter
+    })
+    users.sort(
+      (a, b) =>
+        this.state.ascending
+          ? a[this.state.sort] > b[this.state.sort]
+          : a[this.state.sort] < b[this.state.sort]
+    )
+    return (
+      <Wrapper>
+        <div>
+          <select
+            value={this.state.filter}
+            onChange={e => this.setState({ filter: e.target.value })}
+          >
+            <option value=''>All</option>
+            <option value='active'>Active</option>
+            <option value='inactive'>Inactive</option>
+          </select>
+          <table>
+            <thead>
+              <tr>
+                {headers.map(header => (
+                  <th
+                    onClick={() => this.updateSort(header.key)}
+                    key={header.key}
+                  >
+                    {header.label}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {users.map(user => (
+                <tr key={user.id}>
+                  {headers.map(header => (
+                    <td key={header.key}>{user[header.key]}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </Wrapper>
+    )
+  }
 }
---
+++ src/users.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Bubba Jones',
+    email: 'bubba@jones.fake',
+    office: 'San Diego',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Trixie Dorkl Fussenbinger',
+    email: 'trixie@fussen.fake',
+    office: 'Pittsburg',
+    status: 'active'
+  },
+  {
+    id: 3,
+    name: 'Fritz Mummelhummer',
+    email: 'fritz@mummel.fake',
+    office: 'Pittsburg',
+    status: 'active'
+  },
+  {
+    id: 4,
+    name: 'Myrmal Kizzie Blaukenship',
+    email: 'myrmal@blauken.fake',
+    office: 'San Diego',
+    status: 'inactive'
+  },
+  {
+    id: 5,
+    name: 'Frank Frank',
+    email: 'frank@frank.fake',
+    office: 'Toronto',
+    status: 'inactive'
+  }
+]
