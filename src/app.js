7b0ca8779bea72d6d6bac16b225e24a4b8fb917a
Dallin Osmun <dosmun@gmail.com>
2018-04-27 21:42:24 -0600

    created a basic project template

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  margin-top: 40px;
+  justify-content: center;
+`
+
+export default () => {
+  return <Wrapper>hi</Wrapper>
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+import App from './app'
+
+const el = document.getElementById('app')
+ReactDom.render(<App />, el)
